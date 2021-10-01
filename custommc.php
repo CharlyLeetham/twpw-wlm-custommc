6a16c59c3eec953d3830f273d1773f83df965566
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-01 13:50:57 -1000

    Update custommc.php

--- custommc.php
+++ custommc.php
@@ -535,23 +535,23 @@ if ( !isset ($twpw_custom_mc) ){
 register_activation_hook ( __FILE__, array(&$twpw_custom_mc, 'twpw_custom_mc_activate' ) );
 add_action('admin_head', array (&$twpw_custom_mc, 'twpw_custommc_admin_register_head' ) );
 
-// add_action ( 'wishlistmember_approve_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_add_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_remove_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_unapprove_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_unconfirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_confirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_cancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
-// add_action ( 'wishlistmember_uncancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_approve_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_add_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_remove_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_unapprove_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_unconfirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_confirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_cancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
+add_action ( 'wishlistmember_uncancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_test' ), 30, 2 );
 
-// add_action ( 'wishlistmember_approve_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
-// add_action ( 'wishlistmember_add_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
-// add_action ( 'wishlistmember_confirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
-// add_action ( 'wishlistmember_uncancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
+add_action ( 'wishlistmember_approve_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
+add_action ( 'wishlistmember_add_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
+add_action ( 'wishlistmember_confirm_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
+add_action ( 'wishlistmember_uncancel_user_levels', array( &$twpw_custom_mc, 'acl_wlm_approve_user' ), 30, 2 );
 
-add_action ( 'wishlistmember_remove_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
-add_action ( 'wishlistmember_unapprove_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
-add_action ( 'wishlistmember_unconfirm_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
-add_action ( 'wishlistmember_cancel_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
+// add_action ( 'wishlistmember_remove_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
+// add_action ( 'wishlistmember_unapprove_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
+// add_action ( 'wishlistmember_unconfirm_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
+// add_action ( 'wishlistmember_cancel_user_levels', array ( &$twpw_custom_mc, 'acl_wlm_unapprove_user' ), 30, 2 );
 
 ?>
