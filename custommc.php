4daf62b89d70ea4df56ade8fdbef728fe34f6557
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-01 12:52:54 -1000

    Update custommc.php

--- custommc.php
+++ custommc.php
@@ -150,7 +150,7 @@ function acl_wlm_approve_user( $id, $levels ) {
 			if ( $debug ) {
 				echo "No List"; 
 				echo "\r\n\r\n"; 
-				$logfile = fopen( LOGPATH."mcintlog.log", "a" );
+				$logfile = fopen( LOGPATH."mcapplog.log", "a" );
 				$out =ob_get_clean();
 				fwrite( $logfile, $out );
 				fclose( $logfile );			
@@ -284,7 +284,7 @@ function acl_wlm_approve_user( $id, $levels ) {
 			}
 
 			if ( $debug ) {
-				$logfile = fopen( LOGPATH."mcintlog.log", "a" );
+				$logfile = fopen( LOGPATH."mcapplog.log", "a" );
 				$out =ob_get_clean();
 				fwrite( $logfile, $out );
 				fclose( $logfile );		
@@ -294,6 +294,210 @@ function acl_wlm_approve_user( $id, $levels ) {
 }
 add_action ( 'wishlistmember_approve_user_levels', 'acl_wlm_approve_user', 30, 2 );
 add_action ( 'wishlistmember_add_user_levels', 'acl_wlm_approve_user', 30, 2 );
+add_action ( 'wishlistmember_confirm_user_levels', 'acl_wlm_approve_user', 30, 2 );
+add_action ( 'wishlistmember_uncancel_user_levels', 'acl_wlm_approve_user', 30, 2 );
+
+function acl_wlm_unapprove_user( $id, $levels ) {
+	
+	ob_start();
+	define( 'LOGPATH', dirname( __FILE__ ) . '/logs/' );
+	date_default_timezone_set("US/Hawaii");
+	$logging = true;
+	$debug = true;
+	$logger = '';
+	
+	$settings = get_option("twpw_custommc");
+	$api_key = $settings['mcapikey'];	
+	
+	/* Setup Logging */
+	if (!file_exists(dirname( __FILE__ ).'/logs')) {
+		mkdir(dirname( __FILE__ ).'/logs', 0775, true);
+	}
+	define( 'LOGPATH', dirname( __FILE__ ) . '/logs/' );
+	/* End logging setup */
+	
+	$wlmlevels = wlmapi_get_member_levels($id); //Using the member ID, get the membership level details. We're going to use this information to find those that need approval.	
+
+	if ( $debug ) {
+		echo 'Date: '. date("m/d/Y H:i:s").' ('.date("O").') GMT'."\r\n\r\n";
+		echo "User ID: " .$id;
+		echo "\r\n\r\n";		
+		echo 'Post: ';
+		$postexp = var_export( $_POST, true );
+		echo $postexp;
+		echo "\r\n\r\n";		
+		$levexp = var_export( $levels, true );
+		echo 'Levels: '.$levexp;
+		echo "\r\n\r\n";
+		$levexp = var_export ( $wlmlevels, true );
+		echo 'WLM Levels: '.$levexp;
+		echo "\r\n\r\n";
+		$sett = var_export ( $settings, true );
+		echo 'TWPW CustomMC:';
+		echo $sett."\r\n\r\n";
+	}
+
+	//get the user object so we can grab their details to add to Mailchimp
+	$user = get_user_by( 'id', $id );
+	$firstname = $user->user_firstname;
+	$lastname = $user->user_lastname;
+	$useremail = $user->user_email;
+	$wlmaction = $_POST['WishListMemberAction'];
+	$levelaction = $_POST['level_action'];
+
+	foreach( $levels as $k => $levid ) {
+
+		if ( ( $settings[$levid]['mclistid'] ) ) {
+			$mclistid = $settings[$levid]['mclistid'];
+		} else {
+			$mclistid = false;
+		}
+		
+		if ( $mclistid == false ) {
+			if ( $debug ) {
+				echo "No List"; 
+				echo "\r\n\r\n"; 
+				$logfile = fopen( LOGPATH."mcremlog.log", "a" );
+				$out =ob_get_clean();
+				fwrite( $logfile, $out );
+				fclose( $logfile );			
+			}
+			
+			if ( $logging ) {
+				$logger = date("m/d/Y H:i:s"). '('. date ("O") .' GMT) '.$firstname.' '.$lastname.'('.$id.' '.$levid.') not added to Mailchimp.'."\r\n";
+				$logfile = fopen( LOGPATH."removemember.log", "a" );
+				fwrite( $logfile, $logger );
+				fclose( $logfile );				
+			}
+		} else { 
+			if ( $debug ) {
+				echo "List: " . $mclistid; 
+				echo "\r\n\r\n"; 
+			}
+
+			$double_optin = (empty($settings[$levid]['dblopt']))?true:false;
+			$unsub = (empty($settings[$level]['unsub']))?false:true;
+			$send_welcome = (empty($settings[$levid]['sendwel']))?false:true;
+			$send_goodbye = (empty($settings[$levid]['sendbye']))?false:true;
+			$send_notify = (empty($settings[$levid]['sendnotify']))?false:true;
+			$groupings = array(); // create groupings array
+			if( !empty( $settings[$levid]['mcgroup'] ) ) { // if there are groups
+				foreach( $settings[$levid]['mcgroup'] as $group ) { // go through each group that's been set
+					$group = explode('::',$group); // divide the group as top id and bottom name
+					$groups[$group[0]][] = $group[1]; 
+				}
+				foreach($groups as $group_id => $group) {
+					$groupings[] = array('id'=>$group_id, 'groups' => $group);
+				}
+			}
+			// Setup the array to send to Mailchimp
+			global $wpdb;
+			//$mailchimp = new Mailchimp ( $mcapikey );
+			$merge_vars = array (
+								 'FNAME' => $firstname,
+								 'LNAME' => $lastname,
+								 'GROUPINGS' => $groupings,
+								);
+			$merge_vars = array_merge($merge_vars, $settings[$levid]['merge_vars']);
+
+			// For PDT ONLY
+			$merge_vars['JOINED'] = current_time('Y-m-d');
+			$previous_join_date = get_user_meta( $id, 'wlm_join_date', false );
+			if ( empty ( $previous_join_date ) ) {
+				add_user_meta ( $id, 'wlm_join_date', $merge_vars['JOINED'] );
+				echo 'join date of '.$merge_vars['JOINED'].' added'."\r\n\r\n";
+				$logger1 = 'join date of '.$merge_vars['JOINED'].' added';
+			} elseif ( $settings[$level[$levid]]['update_join_date'] == 'yes' ) {
+				update_user_meta ( $id, 'wlm_join_date', $merge_vars['JOINED'] );
+				echo 'join date updated from '.$previous_join_date.' to: '.$merge_vars['JOINED']."\r\n";			
+				$logger1 = 'join date updated from '.$previous_join_date.' to: '.$merge_vars['JOINED'];			
+			} else {
+				echo 'join date not updated from '.$previous_join_date."\r\n";
+				$logger1 = 'join date not updated from '.$previous_join_date[0];
+			}
+
+			$email_type = 'html';
+			$update_existing = TRUE;
+			$replace_interests = TRUE;	
+			$delete_member = FALSE;
+					
+			if ( $debug ) {
+				$myarray = array(
+					'apikey' => $mcapikey,
+					'id' => $mclistid,
+					'email' => array('email' => $useremail),
+					'delete_member' => $delete_member,
+					'send_goodbye' => $send_goodbye,
+					'send_notify' => $send_notify
+				);
+				$myarr = var_export ( $myarray, true );
+				echo 'Mailchimp settings: '."\r\n\r\n";
+				echo $myarr."\r\n";
+			}
+			
+			if ( $live ) {
+				
+				$result = $mailchimp->call( '/lists/unsubscribe', array(
+					'apikey' => $mcapikey,
+					'id' => $mclistid,
+					'email' => array('email' => $useremail),
+					'delete_member' => $delete_member,
+					'send_goodbye' => $send_goodbye,
+					'send_notify' => $send_notify
+				));
+												
+				if ($mailchimp->errorCode){
+					if ( $logging ) {
+						$logger .= "Unable to load listUnsubscribe()!\n\r";
+						$logger .= "\tCode=".$mailchimp->errorCode."\n\r";
+						$logger .= "\tMsg=".$mailchimp->errorMessage."\n\r";				
+					}
+					
+				} else {
+					if ( $logging ) {
+						$logger .= date("m/d/Y H:i:s"). '('. date ("O") .' GMT) Removed '.$firstname .'('.$id.') for Level: '.$levid.' from Mailchimp List: '.$mclistid. 'by '.$wlmaction.' ('.$levelaction.')'."\r\n";
+						if ( $groupings ) {
+							$logger .= 'for groups: '.var_export( $groupings, true )."\r\n";
+						}
+						$logger .= ' '.$logger1;
+						$logger .= "\n\r---\n\r";
+					}
+				}
+			} else {
+				if ( $debug ) {
+					echo 'Call made: $mailchimp->call( /lists/unsubscribe, '. $myarr .')';
+					echo "\r\n";
+				}
+				
+				if ( $logging ) {
+					$logger .= date("m/d/Y H:i:s"). '('. date ("O") .' GMT) Removed '.$firstname .'('.$id.') for Level: '.$levid.' from Mailchimp List: '.$mclistid. 'by '.$wlmaction.' ('.$levelaction.')'."\r\n";
+					if ( $groupings ) {
+						$logger .= 'for groups: '.var_export( $groupings, true )."\r\n";
+					}
+					$logger .= ' '.$logger1;				
+					$logger .= "\r\n---\r\n";
+				}			
+			}
+		
+			if( $logging ) {
+				$logfile = fopen( LOGPATH."removemember.log", "a" );
+				fwrite( $logfile, $logger );
+				fclose( $logfile );
+			}
+
+			if ( $debug ) {
+				$logfile = fopen( LOGPATH."mcremlog.log", "a" );
+				$out =ob_get_clean();
+				fwrite( $logfile, $out );
+				fclose( $logfile );		
+			}
+		}
+	}
+}
+add_action ( 'wishlistmember_remove_user_levels', 'acl_wlm_unapprove_user', 30, 2 );
+add_action ( 'wishlistmember_unapprove_user_levels', 'acl_wlm_unapprove_user', 30, 2 );
+add_action ( 'wishlistmember_unconfirm_user_levels', 'acl_wlm_unapprove_user', 30, 2 );
+add_action ( 'wishlistmember_cancel_user_levels', 'acl_wlm_unapprove_user', 30, 2 );
 
 function get_mailchimp_lists($mclistid,$wlmlevelid) {
 	//Setup the mailchimp api
