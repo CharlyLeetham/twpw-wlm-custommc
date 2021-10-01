625d9d5112f93e5d3469da694d8181a0384ee76c
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-01 12:29:27 -1000

    Update custommc.php

--- custommc.php
+++ custommc.php
@@ -83,14 +83,14 @@ function acl_wlm_test( $id, $levels ) {
 	fclose( $logfile );	
 }
 
-add_action ( 'wishlistmember_approve_user_levels', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_add_user_levels', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_approve_user_levels', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_add_user_levels', 'acl_wlm_test', 30, 2 );
 add_action ( 'wishlistmember_remove_user_levels)', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_unapprove_user_levels)', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_unconfirm_user_levels)', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_confirm_user_levels)', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_cancel_user_levels)', 'acl_wlm_test', 30, 2 );
-add_action ( 'wishlistmember_uncancel_user_levels)', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_unapprove_user_levels)', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_unconfirm_user_levels)', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_confirm_user_levels)', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_cancel_user_levels)', 'acl_wlm_test', 30, 2 );
+// add_action ( 'wishlistmember_uncancel_user_levels)', 'acl_wlm_test', 30, 2 );
 
 function acl_wlm_approve_user( $id, $levels ) {
 	
