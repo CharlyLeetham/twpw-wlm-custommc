5093d487701d4ccd6e581436897f92af911b0cba
root <root@cp.geekgoddesswebhosting.com>
2021-10-07 00:56:48 +0000

    Add new mailchimp api and tests

---
+++ mctest.php
@@ -0,0 +1,80 @@
+<?php
+$dc = "us1";
+$mcapikey = "c7a1d02e8fbe7e12fee97c9e4909967d-us1";
+$listid = 'c580a5bbc9';
+//$mcapikey = "c7a1d02e8fbe7e12fee97c9e4909967e-us1";
+
+require_once('mailchimp/vendor/autoload.php');
+$mailchimp = new \MailchimpMarketing\ApiClient();
+$mailchimp->setConfig([
+	'apiKey' => $mcapikey,
+	'server' => $dc
+]);
+
+//try {
+	//$response = $mailchimp->ping->get();
+	//echo var_export( $response, true);
+
+//} catch (Exception $e) {
+//	echo '<pre>';
+//	$exception = (string) $e->getResponse()->getBody();
+//        $exception = json_decode($exception);
+//	echo 'An error has occurred: '.$exception->title.' - '.$exception->detail;
+//	echo '</pre>';
+//} finally {
+	//echo 'More code here';
+//}
+
+$subemailhash = md5('helpdesk+test113@askcharlyleetham.com');
+
+if ( $_GET['interests'] ) {
+	try {
+		$response1 = $mailchimp->lists->getListInterestCategories($listid);
+		$mccats = $response1->categories;
+		$catarr = array();
+		$intarr = array();
+		$catnum = 0;
+		echo '<pre>';
+	        foreach ($mccats as $k) {
+			$catarr[$catnum]['id'] = $k->id;
+			$catarr[$catnum]['title'] = $k->title;
+			$catnum++;
+		}
+
+		$intnum = 0;
+		foreach ( $catarr as $cat1 ) {
+			$interests = $mailchimp->lists->listInterestCategoryInterests( $listid, $cat1['id'] );
+			$ia = $interests->interests;
+			foreach ( $ia as $v ) {
+				$intarr[$intnum]['title'] = $v->name;
+				$intarr[$intnum]['id'] = $v->id;
+				$intarr[$intnum]['catid'] = $v->category_id;
+				$intnum++;
+			}
+		}
+	        $response = $mailchimp->lists->updateListMember($listid, $subemailhash, [ 'status' => 'subscribed', 'email_address' => 'helpdesk+test113@askcharlyleetham.com', 'merge_fields' => ['FNAME' => 'Charly'], 'interests' => array ('2fad6d6b73' => true), ]);
+		$response = $mailchimp->lists->getListMember($listid, $subemailhash);
+        	echo 'Response: '.var_export( $response, true).'<br />';
+		echo '</pre>';
+	} catch (Exception $e) {
+        	echo '<pre>';
+	        $exception = (string) $e->getResponse()->getBody();
+        	$exception = json_decode($exception);
+		echo var_export( $exception ).'<br />';
+        	echo 'An error has occurred: '.$exception->title.' - '.$exception->detail;
+        	echo '</pre>';
+	} finally {
+		echo '<pre>';
+		var_dump ($response);
+		echo '</pre>';
+        	echo 'More code here <br />';
+		echo 'kdkdkdkd<br />';
+	}
+} 
+
+if ( $_GET['lists'] ) {
+
+
+
+?>
+
