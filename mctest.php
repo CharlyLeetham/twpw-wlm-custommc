7e4d1e83528d3391aba552eeacf92aef92945172
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-06 15:39:44 -1000

    Update mctest.php

--- mctest.php
+++ mctest.php
@@ -76,7 +76,7 @@ if ( $_GET['lists'] ) {
 		$mclists = $response1->lists;
 		// echo var_export( $mclists, true ).'<br />';;
 		foreach ( $mclists as $list1 ) {
-			echo 'List 1: '.$list1.'<br />';
+			echo 'List 1: '.$list1->id.'<br />';
 			echo var_export( $list1, true ).'<br />';
 			// echo 'List: '.$list1.' - V: '.$v.'<br />';
 			// echo 'ID: '.$list1[0]['id'].'<br />';
