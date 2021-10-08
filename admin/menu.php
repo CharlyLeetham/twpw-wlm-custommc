3435b5b0ed9badeb2f15a90ef65745ac3d316928
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-07 15:02:27 -1000

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -433,8 +433,8 @@ function twpwcustommclists() {
 		}
 		
 	function twpw_get_interest_groups() {
-		// $mclists = twpw_custom_mc::acl_get_interest_groups( $settings[$level['id']]['mclistid'] );
-		echo var_export ( $settings, true );
+		// $mclists = twpw_custom_mc::acl_get_interest_groups( $_POST['mclistid'] );
+		echo $_POST['mclistid'];
 		return;
 		if ( $debug == 'yes' ) {
 			$logger .= "MCGroups: ";
