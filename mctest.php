dd79e4a22bd8f41b6867273618aaf30b345e59b7
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-08 12:08:25 -1000

    Update mctest.php

--- mctest.php
+++ mctest.php
@@ -131,7 +131,7 @@ if ( $_GET['mergevals'] ) {
 		$listnum = 0;
 		echo '<pre>';
 		$mclists = $response1->lists;
-		echo var_export( $mclists, true ).'<br />';;
+		// echo var_export( $mclists, true ).'<br />';
 		foreach ( $mclists as $list1 ) {
 			echo 'List 1: '.$list1->id.'<br />';
 			echo var_export( $list1, true ).'<br />';
