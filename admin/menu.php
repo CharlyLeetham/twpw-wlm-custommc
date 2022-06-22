597c77e390fad43d06268ab486e7196a3badc145
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-06-22 13:02:41 +1000

    ff

--- admin/menu.php
+++ admin/menu.php
@@ -231,7 +231,7 @@ function twpwcustommclists() {
 
 					<!-- List all Mailchimp Lists -->
 					<td><?php
-					echo $twpw_custom_mc->get_mailchimp_lists( $settings[$level['id']]['mclistid'],$level['id'] )
+					echo twpw_custom_mc::get_mailchimp_lists( $settings[$level['id']]['mclistid'],$level['id'] )
 					?></td>
 
 					<!-- List groups for Mailchimp List selected -->
