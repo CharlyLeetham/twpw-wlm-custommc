7ed5abbe71b6e0d511180fce20913f2def6233e4
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-06-22 13:24:01 +1000

    c

--- custommc.php
+++ custommc.php
@@ -631,7 +631,7 @@ class twpw_custom_mc {
 		$api_key = $settings['mcapikey'];
 		if (($api_key <> "")) {
 
-			$mailchimp = twpw_custommc_createMCAPI();
+			$mailchimp = twpw_custom_mc::twpw_custommc_createMCAPI();
 			//$response1 = $mailchimp->ping->get();
 			var_dump ( $mailchimp );
 			die();
