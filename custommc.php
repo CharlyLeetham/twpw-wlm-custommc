e464a76e26d4b40722658ea093f99eee50d75091
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-06-24 09:03:10 +1000

    ff

--- custommc.php
+++ custommc.php
@@ -682,12 +682,14 @@ class twpw_custom_mc {
 
 	public function acl_get_tags( $listid, $levelid, $ajax=null ) {
 		global $twpw_custommc_mcapi;
+		$settings = get_option("twpw_custommc");
 		$response1 = $twpw_custommc_mcapi->lists->tagSearch($listid);
 		$mclists = $response1->tags;
 		$mailchimptags = '<select multiple="multiple" class="mclistid" name="twpw_custommc['.$levelid.'][mctag][]">';
 		foreach ( $mclists as $list1 ) {
 			$mailchimptags.='<option value="'.$list1->id.'"';
-			if ($list1->id == $listid) {
+			/* if ($list1->id == $listid) { */
+			if( in_array( $list1->id, $settings[$levelid['id']]['mctag'] ) ) {
 				$mailchimplists.=' selected="yes" ';
 			}
 			$mailchimptags.='>'.$list1->name.'</option>';
