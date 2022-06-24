81594efaf6f07a23711305d609b2c266bee91f05
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-06-24 14:40:23 +1000

    dd

--- admin/menu.php
+++ admin/menu.php
@@ -614,7 +614,7 @@ if ( $display ) { ?>
 	  wp_die();
 	}
 
-	// add_action( 'wp_ajax_twpw_custommc_ig', 'twpw_get_interest_groups' );
+	add_action( 'wp_ajax_twpw_custommc_ig', 'twpw_get_interest_groups' );
 	add_action( 'wp_ajax_twpw_custommc_tag', 'twpw_get_tags' );
 
 ?>
