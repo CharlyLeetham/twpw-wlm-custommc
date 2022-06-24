20770e589592585dfab7a4acbd64b7e5cc33b6ce
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-06-24 14:28:32 +1000

    ff

--- admin/menu.php
+++ admin/menu.php
@@ -567,10 +567,18 @@ if ( $display ) { ?>
 	function twpw_get_tags() {
 	  $listid =  $_POST['mclistid'];
 	  if ( !empty( $listid ) ) {
-	    global $twpw_custommc_mcapi;
-	    $settings = get_option("twpw_custommc");
+			$acl_plugin_dir = WP_PLUGIN_DIR . '/twpw-wlm-custommc';
+			if (isset($twpw_custommc_mcapi)) return;
+			require_once( $acl_plugin_dir.'/mailchimp/vendor/autoload.php');
+			$settings = get_option("twpw_custommc");
+			$api_key = $settings['mcapikey'];
+			$dc = $settings['mcdc'];
+			$twpw_custommc_mcapi = new \MailchimpMarketing\ApiClient();
+			$twpw_custommc_mcapi->setConfig([
+					'apiKey' => $api_key,
+					'server' => $dc
+			]);
 	    $response1 = $twpw_custommc_mcapi->lists->tagSearch($listid);
-			return($response1);
 	    $mclists = $response1->tags;
 	    echo '<select multiple="multiple" class="mclistid" name="twpw_custommc['.$_POST['levelid'].'][mctag]">';
 	    foreach ( $mclists as $list1 ) {
