635dc31ce1c70d89e369ce4098a29d2cc62faf10
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-28 14:55:10 +1000

    d

--- mctest.php
+++ mctest.php
@@ -174,7 +174,8 @@ if ( $_GET['mergevals'] ) {
 
 if ( $_GET['tags'] ) {
 	try {
-		$response1 = $mailchimp->lists->tagSearch( $listid, [
+		$response1 = $mailchimp->lists->tagSearch( $listid );
+		// $response1 = $mailchimp->lists->tagSearch( $listid, [
 			"offset" => 0,
 			"count" => 1000
 		]);
