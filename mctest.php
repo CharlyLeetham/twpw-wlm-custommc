897aa64cceb1dfb4dc95fdea89f25a6e76156816
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-28 16:17:57 +1000

    d

--- mctest.php
+++ mctest.php
@@ -195,14 +195,14 @@ if ( $_GET['tags'] ) {
 		echo '<pre>';
 		//echo var_export( $response1, true ).'<br />';
 		$mclists = $response1->tags;
-		// echo var_export( $mclists, true ).'<br />';
-		foreach ( $mclists as $list1 ) {
-			echo 'List 1: '.$list1->id.'<br />';
-			// echo var_export( $list1, true ).'<br />';
-			echo 'List: '.$list1->id.' - Name: '.$list1->name.'<br />';
-			echo 'ID: '.$list1[0]['id'].'<br />';
-			echo 'name: '.$list1->name.'<br />';
-		}
+		echo var_export( $mclists, true ).'<br />';
+		// foreach ( $mclists as $list1 ) {
+		// 	echo 'List 1: '.$list1->id.'<br />';
+		// 	// echo var_export( $list1, true ).'<br />';
+		// 	echo 'List: '.$list1->id.' - Name: '.$list1->name.'<br />';
+		// 	echo 'ID: '.$list1[0]['id'].'<br />';
+		// 	echo 'name: '.$list1->name.'<br />';
+		// }
 		echo '</pre>';
 	} catch (Exception $e) {
         	echo '<pre>';
