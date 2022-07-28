3d3a79c1a5df3793a6fa141350232eeb4d624a0a
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-28 11:59:23 +1000

    e

--- mctest.php
+++ mctest.php
@@ -28,19 +28,13 @@ if ( !$_GET['dc'] ) {
 }
 
 require_once('mailchimp/vendor/autoload.php');
-require_once('includes/mctransaction/vendor/autoload.php');
+
 $mailchimp = new \MailchimpMarketing\ApiClient();
 $mailchimp->setConfig([
 	'apiKey' => $mcapikey,
 	'server' => $dc
 ]);
 
-
-$mctransaction = new MailchimpTransactional\ApiClient();
-$mctransaction->setConfig([
-	'apiKey' => $mcapikey,
-	'server' => $dc
-]);
 
 try {
 	$response = $mailchimp->ping->get();
@@ -174,13 +168,13 @@ if ( $_GET['mergevals'] ) {
 if ( $_GET['tags'] ) {
 	try {
 		// $response1 = $mailchimp->lists->tagSearch($listid);
-		$response1 = $mctransaction->tags->list;
+		//$response1 = $mctransaction->tags->list;
 		// $listarr = array();
 		// $listnum = 0;
 		// $totalitems = $response1->total_items;
 		// $response1 = $mailchimp->lists->tagSearch($listid, null, null, $totalitems, '0');
-		echo '<pre>';
-		echo var_export( $response1, true ).'<br />';
+		//echo '<pre>';
+		//echo var_export( $response1, true ).'<br />';
 		// $mclists = $response1->tags;
 		// echo $response1->total_items.'<br />';
 		// echo var_export( $mclists, true ).'<br />';
@@ -191,7 +185,7 @@ if ( $_GET['tags'] ) {
 			// echo 'ID: '.$list1[0]['id'].'<br />';
 			// echo 'name: '.$list1->name.'<br />';
 		// }
-		echo '</pre>';
+	//	echo '</pre>';
 	} catch (Exception $e) {
         	echo '<pre>';
 	        $exception = (string) $e->getResponse()->getBody();
