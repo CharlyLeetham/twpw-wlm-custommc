06ba69d3929669af9a9ec160d9f4cd13ab63d058
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-29 17:26:31 +1000

    d

--- custommc.php
+++ custommc.php
@@ -377,15 +377,15 @@ class twpw_custom_mc {
 						// array_merge($first, $second);
 
 						$logger .= '$twpw_custommc_mcapi->lists->updateListMemberTags('.$mclistid.', '.$subemailhash.', ['."\r\n";
-						foreach ( $tagtags as $t2 ) {
-							$logger .= var_export ( $t2, true )."\r\n\r\n";
+						foreach ( $tagtags as $key, $value) {
+							$logger .= ".$key.". var_export ( $value, true )."\r\n\r\n";
 						}
-						
+
 						$logger .= ']);'."\r\n\r\n";
 
-	  				$response1 = $twpw_custommc_mcapi->lists->updateListMemberTags($mclistid, $subemailhash, [
-							$tagtags
-						]);
+	  				// $response1 = $twpw_custommc_mcapi->lists->updateListMemberTags($mclistid, $subemailhash, [
+						// 	$tagtags
+						// ]);
 
 						$logger .= "\r\n\r\n";
 						$logger .= var_export( $response1, true );
