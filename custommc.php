46e53f6b6a1a31800bc0bb0860eecd63dee06093
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-29 12:34:38 +1000

    e

--- custommc.php
+++ custommc.php
@@ -374,18 +374,18 @@ class twpw_custom_mc {
 						$logger .= '],'. "\r\n";
 					  $logger .= ']);'."\r\n\r\n";
 
-						foreach ( $tags as $t ) {
-							$tagstuff .= '["name" => '.$t["name"].','."\r\n";
-							$tagstuff .= '"status" => '.$t["status"].'],'."\r\n";
-						}
+						// foreach ( $tags as $t ) {
+						// 	$tagstuff .= '["name" => '.$t["name"].','."\r\n";
+						// 	$tagstuff .= '"status" => '.$t["status"].'],'."\r\n";
+						// }
 
 						// $tags = '"tags" => ['.$tagstuff.']';
-						$logger .= var_export ( $tagstuff, true );
+						// $logger .= var_export ( $tagstuff, true );
 	  				// $response1 = $twpw_custommc_mcapi->lists->updateListMemberTags($mclistid, $subemailhash, [
 							// $tags
 						// ]);
 
-						$logger .= $var_export( $response1, true );
+						// $logger .= $var_export( $response1, true );
 					} catch (Exception $e) {
 						$logger .= var_export( $e )."\r\n\r\n";
 						$logger .= $e->getMessage(). "\n";
