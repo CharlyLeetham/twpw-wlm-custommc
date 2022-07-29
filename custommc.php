e87e6503a715dba20d2801e229017a972a23e981
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-29 10:40:51 +1000

    s

--- custommc.php
+++ custommc.php
@@ -330,12 +330,10 @@ class twpw_custom_mc {
 						$response = $mailchimp->ping->get();
 						$logger .= $output."\r\n\r\n";
 					} catch (Exception $e) {
-						$exception = (string) $e->getResponse()->getBody();
-						$exception = json_decode($exception);
-						$logger .= var_export( $exception )."\r\n\r\n";
-
-
-						$logger .= echo 'An error has occurred: '.$exception->title.' - '.$exception->detail."\r\n\r\n";
+						// $exception = (string) $e->getResponse()->getBody();
+						// $exception = json_decode($exception);
+						// $logger .= var_export( $exception )."\r\n\r\n";
+						// $logger .= echo 'An error has occurred: '.$exception->title.' - '.$exception->detail."\r\n\r\n";
 					}
 
 					// $response = $mailchimp->lists->setListMember("9f47bd4d97", "e31206c0d38f9e1461c5a5ac12cab0c8", [
