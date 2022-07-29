b3fb3cd388cd3260ed082b78a5a5fba15a28fe7f
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-29 13:22:30 +1000

    ee

--- custommc.php
+++ custommc.php
@@ -382,8 +382,7 @@ class twpw_custom_mc {
 						$logger .= ']);'."\r\n\r\n";
 
 	  				$response1 = $twpw_custommc_mcapi->lists->updateListMemberTags($mclistid, $subemailhash, [
-							echo $tags
-
+							"tags" => $tagstuff
 						]);
 
 						$logger .= "\r\n\r\n";
