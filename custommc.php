8065d4a8373d9aa4e90b78518a4bb4e2f6d5f6a4
CharlyLeetham <helpdesk@askcharlyleetham.com>
2022-07-30 15:04:18 +1000

    dd

--- custommc.php
+++ custommc.php
@@ -798,13 +798,11 @@ class twpw_custom_mc {
 
 	public function acl_get_mem_groups ( $levid = NULL, $listid = NULL ) {
 
-		if ( $logging ) {
 			$logger .= "We get here \r\n\r\n";
 			// $logger .= var_export( $catarr, true );
 			$logfile = fopen( LOGPATH."cjltest.log", "a" );
 			fwrite( $logfile, $logger );
 			fclose( $logfile );
-		}
 
 	  if ( !$levid || !$listid ) { return; }
 
